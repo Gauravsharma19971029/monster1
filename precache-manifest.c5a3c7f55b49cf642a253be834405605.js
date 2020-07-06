17440b97f02acb3f6d66d4e6bfb8db610291a6ff
unknown <gauravsharma1997100@gmail.com>
2020-07-06 15:55:18 +0530

    Updates

---
+++ precache-manifest.c5a3c7f55b49cf642a253be834405605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf86f70418b5a03ebe6911d4c253e977",
+    "url": "/%20monsters/index.html"
+  },
+  {
+    "revision": "ddf4cacb206b709c4581",
+    "url": "/%20monsters/static/css/main.8099a3fc.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/%20monsters/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%20monsters/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf4cacb206b709c4581",
+    "url": "/%20monsters/static/js/main.96b8c425.chunk.js"
+  },
+  {
+    "revision": "0b8fe77e508290406211",
+    "url": "/%20monsters/static/js/runtime-main.d3097613.js"
+  }
+]);
